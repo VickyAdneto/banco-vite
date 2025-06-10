400ecb63628a23c851c691939a5c4a2ec21cf681
VickyAdneto <vicky@adneto.in>
2025-06-10 13:29:11 +0530

    update produciton buiold issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-   plugins: [
-      react(),
-      tailwindcss(),
-    ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"),
-    },
-  },
-  server: {
-    port: 3000,
-    open: true
-  },
+  plugins: [react(), tailwindcss()],
+  root: '.',
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
-})
+  },
+});
